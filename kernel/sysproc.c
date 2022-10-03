dcc6943bd0a1dd0779cf4dcedbcc5d09321a10f2
hash-roar <1022647354@qq.com>
2022-10-03 01:54:32 +0000

    :sparkles: (page accesss) finish page access

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -7,65 +7,48 @@
 #include "spinlock.h"
 #include "proc.h"
 
-uint64
-sys_exit(void)
-{
+uint64 sys_exit(void) {
   int n;
-  if(argint(0, &n) < 0)
+  if (argint(0, &n) < 0)
     return -1;
   exit(n);
-  return 0;  // not reached
+  return 0; // not reached
 }
 
-uint64
-sys_getpid(void)
-{
-  return myproc()->pid;
-}
+uint64 sys_getpid(void) { return myproc()->pid; }
 
-uint64
-sys_fork(void)
-{
-  return fork();
-}
+uint64 sys_fork(void) { return fork(); }
 
-uint64
-sys_wait(void)
-{
+uint64 sys_wait(void) {
   uint64 p;
-  if(argaddr(0, &p) < 0)
+  if (argaddr(0, &p) < 0)
     return -1;
   return wait(p);
 }
 
-uint64
-sys_sbrk(void)
-{
+uint64 sys_sbrk(void) {
   int addr;
   int n;
 
-  if(argint(0, &n) < 0)
+  if (argint(0, &n) < 0)
     return -1;
-  
+
   addr = myproc()->sz;
-  if(growproc(n) < 0)
+  if (growproc(n) < 0)
     return -1;
   return addr;
 }
 
-uint64
-sys_sleep(void)
-{
+uint64 sys_sleep(void) {
   int n;
   uint ticks0;
 
-
-  if(argint(0, &n) < 0)
+  if (argint(0, &n) < 0)
     return -1;
   acquire(&tickslock);
   ticks0 = ticks;
-  while(ticks - ticks0 < n){
-    if(myproc()->killed){
+  while (ticks - ticks0 < n) {
+    if (myproc()->killed) {
       release(&tickslock);
       return -1;
     }
@@ -75,31 +58,60 @@ sys_sleep(void)
   return 0;
 }
 
-
 #ifdef LAB_PGTBL
-int
-sys_pgaccess(void)
-{
+int sys_pgaccess(void) {
   // lab pgtbl: your code here.
+  uint64 base;
+  int len;
+  uint64 mask_ptr;
+  struct proc *p = myproc();
+  if (argaddr(1, &base) < 0 || argint(2, &len) < 0 ||
+      argaddr(3, &mask_ptr) < 0) {
+    return -1;
+  }
+  uchar masks[100] = {};
+  memset(masks, 0, 100);
+  int m_index = len / 8;
+  int b_index = len % 8;
+  pte_t *first = walk(p->pagetable, base, 0);
+  if (first == 0) {
+    return -1;
+  }
+
+  for (int i = 0; i < len; i++) {
+    pte_t *pte = walk(p->pagetable, base + i * PGSIZE, 0);
+    if (pte != 0) {
+      if (*pte & PTE_V) {
+        masks[m_index] |= (1 << b_index);
+      }
+    }
+    if (b_index == 0) {
+      b_index = 7;
+      m_index--;
+    } else {
+      b_index--;
+    }
+  }
+
+  if (copyout(p->pagetable, mask_ptr, (char *)masks, len / 8 + 1) < 0) {
+    return -1;
+  }
+
   return 0;
 }
 #endif
 
-uint64
-sys_kill(void)
-{
+uint64 sys_kill(void) {
   int pid;
 
-  if(argint(0, &pid) < 0)
+  if (argint(0, &pid) < 0)
     return -1;
   return kill(pid);
 }
 
 // return how many clock tick interrupts have occurred
 // since start.
-uint64
-sys_uptime(void)
-{
+uint64 sys_uptime(void) {
   uint xticks;
 
   acquire(&tickslock);
