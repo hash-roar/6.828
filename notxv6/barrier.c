74b3523cf0d74f46ac5f754fe6ee884d77b5ad80
hash-roar <1022647354@qq.com>
2022-10-09 11:13:14 +0000

    :construction:

--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -10,39 +10,41 @@ static int round = 0;
 struct barrier {
   pthread_mutex_t barrier_mutex;
   pthread_cond_t barrier_cond;
-  int nthread;      // Number of threads that have reached this round of the barrier
-  int round;     // Barrier round
+  int nthread; // Number of threads that have reached this round of the barrier
+  int round;   // Barrier round
 } bstate;
 
-static void
-barrier_init(void)
-{
+static void barrier_init(void) {
   assert(pthread_mutex_init(&bstate.barrier_mutex, NULL) == 0);
   assert(pthread_cond_init(&bstate.barrier_cond, NULL) == 0);
   bstate.nthread = 0;
 }
 
-static void 
-barrier()
-{
+static void barrier() {
   // YOUR CODE HERE
   //
   // Block until all threads have called barrier() and
   // then increment bstate.round.
   //
-  
+  pthread_mutex_lock(&bstate.barrier_mutex);
+  if (++bstate.nthread == nthread) {
+    bstate.nthread = 0;
+    bstate.round++;
+    pthread_cond_broadcast(&bstate.barrier_cond);
+  } else {
+    pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+  }
+  pthread_mutex_unlock(&bstate.barrier_mutex);
 }
 
-static void *
-thread(void *xa)
-{
-  long n = (long) xa;
+static void *thread(void *xa) {
+  long n = (long)xa;
   long delay;
   int i;
 
   for (i = 0; i < 20000; i++) {
     int t = bstate.round;
-    assert (i == t);
+    assert(i == t);
     barrier();
     usleep(random() % 100);
   }
@@ -50,9 +52,7 @@ thread(void *xa)
   return 0;
 }
 
-int
-main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
   pthread_t *tha;
   void *value;
   long i;
@@ -68,10 +68,10 @@ main(int argc, char *argv[])
 
   barrier_init();
 
-  for(i = 0; i < nthread; i++) {
-    assert(pthread_create(&tha[i], NULL, thread, (void *) i) == 0);
+  for (i = 0; i < nthread; i++) {
+    assert(pthread_create(&tha[i], NULL, thread, (void *)i) == 0);
   }
-  for(i = 0; i < nthread; i++) {
+  for (i = 0; i < nthread; i++) {
     assert(pthread_join(tha[i], &value) == 0);
   }
   printf("OK; passed\n");
